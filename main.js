574a4dc9cfc2a07ffcd5ebf0ed108b051a3e8da8
rudmoon <96547605+rudmoon@users.noreply.github.com>
2023-04-14 20:59:19 +0900

    230414_2
    230414_2

---
+++ main.js
@@ -0,0 +1,52 @@
+const types = document.querySelector('.aside__types');
+const list = document.querySelectorAll('.aside__types li a');
+const listLength = list.length;
+const lists = document.querySelector('.article__lists');
+
+types.addEventListener('click',(ev) => {
+    if(ev.target == null) {
+        return;
+    }
+    ev.preventDefault();
+    
+
+    while(lists.firstChild) {
+        lists.removeChild(lists.firstChild);
+    }
+    const target = ev.target;
+    const href = target.getAttribute('href');
+    const type = href.substr(2);
+
+    for(let i=0; i<listLength; i++) {
+        list[i].classList.remove('active');
+        target.classList.add('active');
+    }
+
+    console.log(type);
+    
+    fetch("./dummy.json")
+        .then(response => response.json())
+        .then((data) => {
+            data.forEach((element,idx) => {
+                const li = document.createElement('li');
+                li.style.backgroundImage = `url('./img/${element.name}.jpg')`;
+                lists.appendChild(li);
+                
+                if(type == 'all') {
+                    li.classList.add('show');
+                } else if(type == element.type) {
+                    li.classList.add('show');
+                }
+            });
+        })
+        .catch(error => console.log(error));
+});
+
+window.onload = function() {
+    let allButton = document.querySelector('.type:first-child a');
+    allButton.click();
+}
+
+
+
+
